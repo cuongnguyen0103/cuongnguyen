831ab241864b32608030f5e69660f0b68a5a51a2
cuongnguyen-hub <cuongnv0103@gmail.com>
2025-01-14 15:02:45 +0700

    update 14.01.2025

--- bai5_mang/Program.cs
+++ bai5_mang/Program.cs
@@ -22,22 +22,23 @@
                 Console.Write(arr[i] + " ");
             }
             Console.WriteLine();
-            //// 2. In mảng dữ liệu trên theo chiều đảo ngược.
-            //Console.WriteLine("2. In mảng dữ liệu trên theo chiều đảo ngược.");
-            //Console.WriteLine("Cach 1: Dung for");
-            //for (int i = n-1;i>=0; i--)
-            //{
-            //    Console.Write(arr[i] + " ");
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine("Cach 2: Dung ham Reverse");
-            //Array.Reverse(arr);
-            //for (int i = 0; i < n; i++)
-            //{
-            //    Console.Write(arr[i] + " ");
-            //}
-            // 3. Tìm số phần tử giống nhau trong mảng và hiển thị số lượng giống nhau ra màn hình.
-            Console.WriteLine("3. Tìm số phần tử giống nhau trong mảng và hiển thị số lượng giống nhau ra màn hình.");
+            // 2. In mảng dữ liệu trên theo chiều đảo ngược.
+            Console.WriteLine("2. In mảng dữ liệu trên theo chiều đảo ngược.");
+            Console.WriteLine("Cach 1: Dung for");
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Cach 2: Dung ham Reverse");
+            int[] arr_reverse = arr;
+            Array.Reverse(arr_reverse);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr_reverse[i] + " ");
+            }
+            // 3.Tìm số phần tử giống nhau trong mảng và hiển thị số lượng giống nhau ra màn hình.
+           Console.WriteLine("3. Tìm số phần tử giống nhau trong mảng và hiển thị số lượng giống nhau ra màn hình.");
             int[] arr_ = new int[n];
             for (int i = 0;i<n; i++)
             {
